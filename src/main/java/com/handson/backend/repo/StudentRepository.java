8e1208a91246d9acaec576de7b863c5137902ecd
Ea125 <altmaneviatar@gmail.com>
2025-09-30 11:34:59 +0300

    feat: with sat filter

--- src/main/java/com/handson/backend/repo/StudentRepository.java
+++ src/main/java/com/handson/backend/repo/StudentRepository.java
@@ -2,6 +2,11 @@ package com.handson.backend.repo;
 
 import com.handson.backend.model.Student;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+@Repository
 public interface StudentRepository extends CrudRepository<Student,Long> {
+
+    List<Student> findAllBySatScoreGreaterThan(Integer satScore);
 }
